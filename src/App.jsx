dbcbaf648e79ba654254d613021eeca1ac0faaa3
Leonardo Recalde <leo03recalde@gmail.com>
2024-09-09 20:17:35 -0300

    punto 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Hola from './components/Hola';
 
 function App() {
   
@@ -6,6 +7,7 @@ function App() {
   return (
     <section className='container'>
       <h1 className='text-center pt-5 display-1'>Hello World!</h1>
+      <Hola saludar ='my friend!'></Hola>
     </section>
   )
 }
---
+++ src/components/Hola.jsx
@@ -0,0 +1,11 @@
+
+
+const Hola = ({saludar}) => {
+    return (
+        <div>
+            <h1 className='text-center pt-5 display-1'>Hello {saludar}</h1>
+        </div>
+    );
+};
+
+export default Hola;
