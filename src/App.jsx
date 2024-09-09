b4703be5fc376775512d22b984aa9fb6f7a2ccf0
Leonardo Recalde <leo03recalde@gmail.com>
2024-09-09 20:07:31 -0300

    hice punto 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <section className='container'>
+      <h1 className='text-center pt-5 display-1'>Hello World!</h1>
+    </section>
   )
 }
 
