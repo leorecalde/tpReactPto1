bb66dce95cfeb7d8677f28a0289ae3f9ccc82e7b
Leonardo Recalde <leo03recalde@gmail.com>
2024-09-10 11:02:47 -0300

    solo es punto 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Hola from './components/Hola';
 
 function App() {
   
@@ -7,7 +6,6 @@ function App() {
   return (
     <section className='container'>
       <h1 className='text-center pt-5 display-1'>Hello World!</h1>
-      <Hola saludar ='my friend!'></Hola>
     </section>
   )
 }
